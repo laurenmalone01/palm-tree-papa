aa0f6c96bef82608e353f39e186bab3a5447e843
Lauren Malone <lauren.malone@devtegrate.com>
2023-01-19 16:12:48 -0600

    commit msg

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000; 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+console.log('in the node console');
+
+app.listen(3000, () => {
+    console.log(`Server is running & listening on port: ${PORT}`);
+  });
+
